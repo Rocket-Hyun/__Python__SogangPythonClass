563bf6f5a79a4dbb0c015138132ce4f834137fd9
Junghyun Park <rocket@likelion.org>
2017-09-21 09:31:11 +0900

    histroy 추가중

--- Assignment/project4.py
+++ Assignment/project4.py
@@ -1,38 +1,14 @@
-import urllib.request
-
-url = "http://paulgraham.com/hp.html"
-f = urllib.request.urlopen(url)
-data = f.read().decode('utf-8')
-
-begin = data.find("May 2003")
-end = data.find("unexpectedly sharp curves.")
-
-print (begin)
-print (end)
-
-end += len ("unexpectedly sharp curves.")
-print (end)
-print (data[begin:begin+100])
-print (data[end-100:end])
-print ("length=", end-begin)
-
-speech = data[begin:end]
-speech = speech.replace("&nbsp;", "")
-speech = speech.replace("<br>", "")
-speech = speech.replace(".", " ")
-speech = speech.replace(",", " ")
-
-speech = speech.split()
-
+textFile = open("howtostartastartup.txt", "r")
+text = textFile.read()
+wordsArray = text.split()
 analyze = {}
-for word in speech:
+for word in wordsArray:
     analyze[word] = analyze.get(word, 0) + 1
-
-flist = sorted(analyze.items(), key=lambda kv: kv[1], reverse=True)
-print("number of words is ", len(flist))
+sortedAnalyze = sorted(analyze.items(), key=lambda kv:kv[1], reverse=True)
+#print(sortedAnalyze[:30])
 
 cnt = 0
-for k, v in flist:
+for k, v in sortedAnalyze:
     print(k, v)
     if cnt > 100: break
     cnt += 1
