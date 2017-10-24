33dd657067d6e6b92c495a6897bbb6e2b34c7347
Rocket-Hyun <rockethyun@gmail.com>
2017-10-24 09:08:24 +0900

    pytho midterm

--- _practice/midterm/midterm.py
+++ _practice/midterm/midterm.py
@@ -1,11 +1,12 @@
 #임포트시
-from textreader.filereader import TextReader
+# from textreader.filereader import TextReader
 
 #하드코딩시
-# class TextReader:
-#     def __init__(self, fileName):
-#         textFile = open(fileName, "r", encoding="utf8")
-#         self.text = textFile.read()
+# 텍스트 파일을 불러오는 클래스
+class TextReader:
+    def __init__(self, fileName):
+        textFile = open(fileName, "r", encoding="utf8")
+        self.text = textFile.read()
 
 #쓸만한 함수
 #문자열.replace("&nbsp;", "")
@@ -14,16 +15,22 @@ from textreader.filereader import TextReader
 #del 리스트[index]
 #리스트.remove('값')
 #리스트.extend(리스트)
+#list(set(리스트)) -> 리스트 중복제거
 
 
-firstFile = TextReader("KakaoTalkChats.txt")
-print(firstFile.text.split(" ")[0])
-secondFile = TextReader("KakaoTalkChats.txt")
-print(secondFile.text[0:50])
+firstFile = TextReader("dummy1.txt")
+print(firstFile.text.split(",")[0:10])
+firstList =firstFile.text.split(",")
+print(len(firstList))
+secondFile = TextReader("dummy2.txt")
+print(secondFile.text.split(",")[0:10])
+secondList = secondFile.text.split(",")
+print(len(secondList))
 
-# CommonWords = []
-# for word in firstFile.text.split(" "):
-#     if word in secondFile.text.split(" "):
-#         CommonWords.append(word)
-#
-# print(CommonWords[0:10])
+# 공통 단어를 뽑아내는 함수
+CommonWords = []
+for word in firstList:
+    if word in secondList:
+        CommonWords.append(word)
+
+print(list(set(CommonWords)))
